b997907329308970c9974f6d73031acbb2eeac06
dsych <dmytrosych834@gmail.com>
2021-06-16 00:24:45 -0400

    jotting down initial logic for coin retrieval and processing

--- src/services/mockDataService.js
+++ src/services/mockDataService.js
@@ -1,3 +1,127 @@
+// path: `/v2/assets/bitcoin/history?interval=d1&start=${before}&end=${now}`,
+//
+const baseUrl = "api.coincap.io/v2";
+
+/**
+ * Retrieves top n number of coins based on their market cap in descending order
+ */
+const retrieveTopCoins = async (limit, excludeSymbolList) => {
+  // it is more efficient to work with a map than with a list
+  const ex = (excludeSymbolList || []).reduce(
+    (acc, el) => (acc[el] = true),
+    {}
+  );
+  let result = [];
+
+  try {
+    const { data } = await fetch(`${baseUrl}/assets`, {
+      method: "GET",
+      redirect: "follow",
+    });
+
+    for (const coin of data) {
+      if (!ex[coin["symbol"]]) {
+        result.push(coin);
+      }
+      // terminate if we got enough coins
+      if (result.size() == limit) {
+        break;
+      }
+    }
+  } catch (err) {
+    console.error("Failed to retrieve top coints", err);
+  }
+
+  return result;
+};
+
+const offsetMonthsFromNow = (months) => {
+  const now = new Date();
+
+  if (now.getMonth() - months < 0) {
+    // if the difference between current month and the offset is negative, we want to add it. otherwise we will perform a double negation
+    now.setFullYear(
+      now.getFullYear() - 1,
+      11 + (now.getMonth() - months),
+      now.getDate()
+    );
+  } else {
+    now.setMonth(now.getMonth() - months);
+  }
+
+  return now;
+};
+
+/**
+ * Ranks coins in ascending order based on their average deviation from the moving average of the past 21 days
+ */
+const rankCoins = async (coinList) => {
+  coinList = coinList || [];
+  startUnixTime = offsetMonthsFromNow(2).getTime();
+  endUnixTime = new Date().getTime();
+
+  // averages: [], deviations: [], averageDeviation: 0, rank: 0
+  const result = {};
+
+  for (const coin of coinList) {
+    try {
+      const {
+        data,
+      } = await fetch(
+        `${baseUrl}/assets/${coin.id}/history?interval=d1&start=${startUnixTime}&end=${endUnixTime}`,
+        { method: "GET", redirect: "follow" }
+      );
+
+      // too little data
+      if (data.size() < 42) {
+        continue;
+      }
+
+      // calculate the initial average
+      let sumOfPeriod = data
+        .slice(-41, -21)
+        .reduce((acc, curr) => (acc += curr.priceUsd), 0);
+
+      // account for possible duplicates
+      result[coin.symbol].deviations = [];
+      result[coin.symbol].averages = [];
+
+      // calculate moving averages for each day of the last 21 days using sliding window
+      for (let i = data.size() - 21; i < data.size(); i++) {
+        // move the average to include the current time point
+        sumOfPeriod += data[i].priceUsd;
+        sumOfPeriod -= data[i - 1].priceUsd;
+
+        const movingAverage = sumOfPeriod / 21;
+
+        result[coin.symbol].averages.push(movingAverage);
+        result[coin.symbol].deviations.push(
+          Math.abs(Math.abs(movingAverage) - Math.abs(data[i].priceUsd))
+        );
+      }
+
+      result[coin.symbol].averageDeviation =
+        result[coin.symbol].deviations.reduce((acc, curr) => (acc += curr), 0) /
+        result[coin.symbol].deviations.size();
+    } catch (err) {
+      console.error("Failed to fetch historical data for ", coin.id);
+      console.error(err);
+    }
+  }
+
+  // we want to sort coins in ascending order based on their averageDeviation, so that we can record their rank
+  const rankedCoins = Object.keys(result)
+    .map((key) =>
+      Object.create({ symbol: key, averageDev: result[key].averageDeviation })
+    )
+    .sort((left, right) => left.averageDev < right.averageDev);
+
+  // record the rank based on the position of sorted symbol
+  rankedCoins.forEach((el, index) => (result[el.symbol].rank = index + 1));
+
+  return result;
+};
+
 export default class MockDataService {
   _breakDowns = {
     1: {
@@ -355,5 +479,10 @@ export default class MockDataService {
   getHoldings(riskLevel) {
     //TODO
     return this._breakDowns[riskLevel];
+  }
+
+  getTopStableCoins() {
+    // retrieve top 10 coins and rank them based on their volatility
+    return retrieveTopCoins(10).then((coins) => rankCoins(coins));
   }
 }
