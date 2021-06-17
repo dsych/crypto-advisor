ba4d5b960005952c09689504fbd00593215a39b6
Insaf Khamzin <insaf.khamzin@ceridian.com>
2021-06-16 22:31:04 -0400

    earnings dynamic

--- src/components/ExpectationChart.js
+++ src/components/ExpectationChart.js
@@ -1,41 +1,106 @@
 import React from 'react';
-import { Box, FormLabel } from '@chakra-ui/react';
-import { AreaChart, Area, ResponsiveContainer, XAxis, Tooltip } from 'recharts';
+import { Box, FormLabel, Text } from '@chakra-ui/react';
+import {
+  AreaChart,
+  Area,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
 
-const rangeData = [
-  {
-    year: '2021',
-    usd: [100, 100],
-  },
-  {
-    year: '2022',
-    usd: [150, 230],
-  },
-  {
-    year: '2023',
-    usd: [240, 400],
-  },
-  {
-    year: '2024',
-    usd: [350, 600],
-  },
-  {
-    year: '2025',
-    usd: [480, 900],
-  },
-];
+const getMarginAmount = (amount, riskLevel) => {
+  const margin = 10; //10% base margin
+  return (amount / 100) * (margin + riskLevel * 2);
+};
 
-export default function ExpectationChart() {
+const getYearIncome = (currentAmount, monthlyDeposit) => {
+  for (let i = 0; i < 12; i++) {
+    //4% a month
+    currentAmount *= 1.04;
+    //+ monthly deposit
+    currentAmount += monthlyDeposit;
+  }
+  return currentAmount;
+};
+
+export default function ExpectationChart({ monthlyDeposit, riskLevel }) {
+  monthlyDeposit = Number.parseFloat(monthlyDeposit);
+
+  const year1 = getYearIncome(monthlyDeposit, monthlyDeposit);
+  const year2 = getYearIncome(year1, monthlyDeposit);
+  const year3 = getYearIncome(year2, monthlyDeposit);
+  const year4 = getYearIncome(year3, monthlyDeposit);
+  const year5 = getYearIncome(year4, monthlyDeposit);
+
+  const year5minusMargin = year5 - getMarginAmount(year5, riskLevel);
+  const year5plusMargin = year5 + getMarginAmount(year5, riskLevel);
+
+  const currentYear = new Date().getFullYear();
+  const rangeData = [
+    {
+      year: currentYear + 1,
+      usd: [
+        year1 - getMarginAmount(year1, riskLevel),
+        year1 + getMarginAmount(year1, riskLevel),
+      ],
+    },
+    {
+      year: currentYear + 2,
+      usd: [
+        year2 - getMarginAmount(year2, riskLevel),
+        year2 + getMarginAmount(year2, riskLevel),
+      ],
+    },
+    {
+      year: currentYear + 3,
+      usd: [
+        year3 - getMarginAmount(year3, riskLevel),
+        year3 + getMarginAmount(year3, riskLevel),
+      ],
+    },
+    {
+      year: currentYear + 4,
+      usd: [
+        year4 - getMarginAmount(year4, riskLevel),
+        year4 + getMarginAmount(year4, riskLevel),
+      ],
+    },
+    {
+      year: currentYear + 5,
+      usd: [year5minusMargin, year5plusMargin],
+    },
+  ];
+
   return (
-    <Box h="250px">
-      <FormLabel>In 5 years we expect you to have</FormLabel>
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart width={700} height={250} data={rangeData}>
-          <XAxis dataKey="year" />
-          <Area type="monotone" dataKey="usd" stroke="#8884d8" fill="#8884d8" />
-          <Tooltip />
-        </AreaChart>
-      </ResponsiveContainer>
+    <Box>
+      <Box>
+        <FormLabel>In 5 years you will likely have</FormLabel>
+        <Text>
+          ${year5minusMargin.toFixed(2)}-{year5plusMargin.toFixed(2)}
+        </Text>
+      </Box>
+      <Box h="300px">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart data={rangeData}>
+            <XAxis dataKey="year" interval="preserveStartEnd" />
+            <YAxis domain={[0, year5 * 1.4]} hide={true} />
+            <Area
+              type="monotone"
+              dataKey="usd"
+              name="$"
+              stroke="#3182ce"
+              fill="#4299e199"
+            />
+            <Tooltip
+              formatter={(value) =>
+                `${value[0].toFixed(2)} - ${value[1].toFixed(2)}`
+              }
+              position={{ x: 'auto', y: 100 }}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </Box>
     </Box>
   );
 }
